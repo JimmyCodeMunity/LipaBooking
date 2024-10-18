5674178d75598d0e4ca049f5c02feb1156730c6a
Jimmy <96749667+JimmyCodeMunity@users.noreply.github.com>
2024-10-18 12:33:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ if (process.env.NODE_ENV !== 'PRODUCTION') {
     })
 }
 
-// const port = process.env.PORT;
-// const dbconnection = process.env.DB_CONNECTION
+const port = process.env.PORT;
+const dbconnection = process.env.DB_CONNECTION
 
 app.listen(port, (req, res) => {
     console.log(`Server is running on port ${port}`)
